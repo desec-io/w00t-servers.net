fa6510cb4ae75cfe6a8ee40c4027d170ffcc9257
Peter Thomassen <peter@desec.io>
2026-02-07 03:37:28 +0100

    Inject original record in bitflip response

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -v ./conf:/config -v ./zones:/storage -p 53:53/udp -p 53:53/tcp --name knot -d cznic/knot knotd
+docker run --rm -v ./conf:/config -v ./zones:/storage -p 53:53/udp -p 53:53/tcp --name knot -d cznic/knot:mod-append-3.5 knotd
